373989c7efc32c2b13323bb0e4a6d60f9818e723
fabischkamau <126986727+fabischkamau@users.noreply.github.com>
2025-01-01 22:38:48 +0300

    unused import

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,7 +1,2 @@
-export function sayHello(name: string | null = null): string {
-  return `Hello, ${name || "World"}!`;
-}
-
-// some
 export * from "./knowledge-graph";
-export * from "./example-tools";
+
